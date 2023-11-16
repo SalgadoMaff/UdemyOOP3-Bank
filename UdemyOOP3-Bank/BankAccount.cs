7a459a352650eb4243aeec2844ad95c96bcad048
Felipe <lipesmaffini@gmail.com>
2023-11-15 22:00:41 -0400

    Add project files.

---
+++ UdemyOOP3-Bank/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿
+namespace UdemyOOP3_Bank {
+    internal class BankAccount {
+        private int _id;
+        public string Titular { get; set; }
+        public double Balance { get; set; }
+
+        public void SetId(int id) {
+            this._id = id;
+        }
+
+        public void Deposit(double income) {
+            this.Balance += income;
+        }
+        public void Withdraw(double withdraw) {
+            this.Balance -= withdraw;
+            this.Balance -= 5;
+        }
+
+    }
+}
